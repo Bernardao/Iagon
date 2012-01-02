61f788c8ff1dbc20e4e93d80f9cf94c78283a04b
Iago Sanjurjo <i_sanjurjo@yahoo.es>
2012-01-01 23:59:51 -0300

    Capitulo 4 hecho

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -1,5 +1,5 @@
 <?php
-#src/Cupon/OfertaBundle/Controller/DefaultController.php
+// src/Cupon/OfertaBundle/Controller/DefaultController.php
 namespace Cupon\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller{
     
     public function ayudaAction(){
-        return new Response('Ayuda');
-        #return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
+        #return new Response('Ayuda');
+        return $this->render('OfertaBundle:Default:ayuda.html.twig');
     }
 }
